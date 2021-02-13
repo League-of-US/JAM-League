be955799f88d1c214781d689e6b723dd7b08d786
Ubuntu <theetails@MC-Server-Gigan.jagmy2clgkoulp1otcvuqspj2h.bx.internal.cloudapp.net>
2021-02-13 05:44:15 +0000

    Taa raahh rahh boom de aye

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(JAM-League "$0")"
-exec java -Xmx6G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar minecraft_server.1.12.2.jar nogui
+exec java -Xmx6G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.12.2-14.23.5.2854.jar nogui
