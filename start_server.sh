0487c33b1c7f98c39d5b5ee5fede33c1281c4ed8
root <root@MC-Server-Gigan.jagmy2clgkoulp1otcvuqspj2h.bx.internal.cloudapp.net>
2021-02-13 02:32:31 +0000

    cmd for linux fix

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-exec java -Xms3G -Xmx6G -jar server.jar --nogui
+cd "$(JAM-League "$0")"
+exec java -Xmx6G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar minecraft_server.1.12.2.jar nogui
